cf36f924bee23fba742728a7de0cc56b2b7b1143
stergiosbamp <bampakis.stergios@gmail.com>
2021-04-01 17:27:44 +0300

    Replacing classes numbers to their meaning for meaningful pie chart

--- imbalance/eda.py
+++ imbalance/eda.py
@@ -4,19 +4,28 @@ import plotly.express as px
 
 class EDA:
 
-    def __init__(self, df):
+    def __init__(self, df, target):
         self.df = df
+        self.target = target
 
-    def pie_chart(self, name):
+    def pie_chart_class_distr(self):
+        # Replace numbers with their meaning for visualization reasons
+        fetal_health_classes = self.df[self.target]
+        fetal_health_classes.replace({
+            1: 'Normal',
+            2: 'Suspect',
+            3: 'Pathological'
+        }, inplace=True)
+
         title = 'Class distribution of fetal Cardiotocograms'
 
-        fig = px.pie(self.df, names=name, hole=0.2, title=title)
+        fig = px.pie(fetal_health_classes, names=self.target, hole=0.2, title=title, width=800, height=800,
+                     color_discrete_sequence=px.colors.qualitative.Safe)
         fig.show()
 
 
 if __name__ == '__main__':
     df = pd.read_csv('../datasets/fetal_health.csv')
 
-    eda = EDA(df=df)
-    eda.pie_chart('fetal_health')
-
+    eda = EDA(df=df, target='fetal_health')
+    eda.pie_chart_class_distr()
