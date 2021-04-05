974a8dcad8b4b5fd7d4ddd7f54300ef69a61778f
stergiosbamp <bampakis.stergios@gmail.com>
2021-04-05 15:46:18 +0300

    Created 2 classes per imbalanced/balanced models and cross-validation. Also added f1 score in SCORINGS

--- imbalance/experiments.py
+++ imbalance/experiments.py
@@ -1,36 +1,37 @@
 from imblearn.over_sampling import SMOTE, ADASYN, BorderlineSMOTE
-from imblearn.pipeline import Pipeline, make_pipeline
+from imblearn.under_sampling import NearMiss, RandomUnderSampler, TomekLinks
+from imblearn.pipeline import Pipeline
 from imblearn.metrics import geometric_mean_score, classification_report_imbalanced
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_validate, cross_val_score, StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.model_selection import cross_validate
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import balanced_accuracy_score, make_scorer, f1_score, roc_auc_score
-from sklearn.linear_model import LogisticRegression
-from itertools import combinations, product, permutations
+from sklearn.metrics import balanced_accuracy_score, make_scorer, f1_score
 
 from dataloader import DataLoader
 
 
 class Modelling:
 
-    def __init__(self, classifier, resampler):
+    def __init__(self, classifier):
         self.x, self.y = DataLoader().get_x_y()
         # self.x_train, self.x_test, self.y_train, self.y_test = DataLoader().get_train_test()
 
         self.classifier = classifier
-        self.resampler = resampler
 
     def run(self, scoring):
-        # Note the Pipeline from "imblearn" to avoid data leakage!
-        
-        # To obtain metrics such as roc_auc we might must to manually iterate on CV splits
-        # since it can't be applied directly on multi-class problems
-        
+        raise NotImplemented('Implement concrete class of Modelling')
+
+
+class ImbalancedModelling(Modelling):
+
+    def __init__(self, classifier):
+        super().__init__(classifier)
+
+    def run(self, scoring):
         model = Pipeline([
-            ('r', self.resampler),
-            ('s', StandardScaler()),
-            ('c', self.classifier)
+            ('scaler', StandardScaler()),
+            ('classifier', self.classifier)
         ])
         results = cross_validate(estimator=model,
                                  X=self.x,
@@ -39,19 +40,70 @@ class Modelling:
                                  scoring=scoring,
                                  n_jobs=-1)
 
-        print("For classifier: {} with resampler: {}".format(self.classifier, self.resampler))
         for score in scoring.keys():
             cv_key = 'test_' + score  # construct the key of the cv to obtain avg
-            print("\tAverage {} is: {}".format(score, results[cv_key].mean()))
-        
+            print("\t\tAverage {} is: {}".format(score, results[cv_key].mean()))
+
+    def __str__(self):
+        return 'ImbalancedModelling({})'.format(clf)
+
+
+class BalancedModelling(Modelling):
+
+    def __init__(self, classifier, resampler):
+        super().__init__(classifier)
+        self.resampler = resampler
+
+    def run(self, scoring):
+        # Note the Pipeline from "imblearn" to avoid data leakage!
+
+        # To obtain metrics such as roc_auc we might have to manually iterate on CV splits
+        # since it can't be applied directly on multi-class problems
+
+        model = Pipeline([
+            ('resampler', self.resampler),
+            ('scaler', StandardScaler()),
+            ('classifier', self.classifier)
+        ])
+        results = cross_validate(estimator=model,
+                                 X=self.x,
+                                 y=self.y,
+                                 # cv=cv,
+                                 scoring=scoring,
+                                 n_jobs=-1)
+
+        for score in scoring.keys():
+            cv_key = 'test_' + score  # construct the key of the cv to obtain avg
+            print("\t\tAverage {} is: {}".format(score, results[cv_key].mean()))
+
+    def __str__(self):
+        return 'BalancedModelling({}, {})'.format(clf, self.resampler)
+
 
 if __name__ == '__main__':
+
+    # Define constants for the experiments
+    SCORING = {
+        'b_acc': make_scorer(balanced_accuracy_score),
+        'g_mean': make_scorer(geometric_mean_score),
+        'f1': make_scorer(f1_score, average='macro')
+    }
     UPSAMPLERS = [SMOTE(random_state=4), BorderlineSMOTE(random_state=4), ADASYN(random_state=4)]
-    random_forest = RandomForestClassifier(n_estimators=200, random_state=4)
+    DOWNSAMPLERS = [TomekLinks(), RandomUnderSampler(random_state=4)]
+    CLASSIFIERS = [
+        RandomForestClassifier(n_estimators=200, random_state=4),
+        GradientBoostingClassifier(n_estimators=200, random_state=4, max_depth=10, max_features='auto')
+    ]
 
     for upsampler in UPSAMPLERS:
-        modelling = Modelling(random_forest, upsampler)
-        modelling.run(scoring={
-            'b_acc': make_scorer(balanced_accuracy_score),
-            'g_mean': make_scorer(geometric_mean_score)
-        })
+        print("Upsampler: {}".format(upsampler))
+        for clf in CLASSIFIERS:
+            imb_model = ImbalancedModelling(clf)
+            print('\t{}'.format(imb_model))
+            imb_model.run(scoring=SCORING)
+
+            bal_model = BalancedModelling(clf, TomekLinks())
+            print('\t{}'.format(bal_model))
+            bal_model.run(scoring=SCORING)
+
+    # TODO: Do the same loop for DOWNSAMPLERS
