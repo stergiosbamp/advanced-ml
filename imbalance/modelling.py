f321bd9ca99533698333021ce7af05ee9470c262
stergiosbamp <bampakis.stergios@gmail.com>
2021-04-07 22:25:12 +0300

    Implement a deep learning MLP model

--- imbalance/modelling.py
+++ imbalance/modelling.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from imblearn.over_sampling import SMOTE, ADASYN, BorderlineSMOTE
 from imblearn.under_sampling import NearMiss, RandomUnderSampler, TomekLinks
 from imblearn.pipeline import Pipeline
@@ -7,6 +9,11 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.model_selection import cross_validate
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import balanced_accuracy_score, make_scorer, f1_score
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Input
+from keras.metrics import AUC
+
 
 from dataloader import DataLoader
 
@@ -78,6 +85,39 @@ class BalancedModelling(Modelling):
 
     def __str__(self):
         return 'BalancedModelling({}, {})'.format(clf, self.resampler)
+
+
+class DeepModelling:
+
+    def __init__(self):
+        self.x_train, self.x_test, self.y_train, self.y_test = DataLoader().get_train_test()
+
+        # One-hot encode target
+        self.y_train = pd.get_dummies(self.y_train)
+        self.y_test = pd.get_dummies(self.y_test)
+
+        self.model = self.__init_model()
+
+    def run(self):
+        history = self.model.fit(x=self.x_train, y=self.y_train, batch_size=64, epochs=100)
+        # TODO: Plot history losses via Evaluator
+
+        results = self.model.evaluate(self.x_test, self.y_test, return_dict=True)
+        print(results)
+
+    def __init_model(self):
+        model = Sequential()
+
+        model.add(Input(shape=(self.x_train.shape[1],)))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.2))
+        model.add(Dense(3, activation='softmax'))
+
+        model.compile(optimizer='adam', metrics=[AUC()], loss='categorical_crossentropy')
+
+        return model
 
 
 if __name__ == '__main__':
