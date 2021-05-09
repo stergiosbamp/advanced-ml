83e18bcd16cfa31fc7f855e4ad3d5468dd5dc464
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-05-09 11:43:47 +0300

    Refactored model fine tuning

--- multilabel/fine_tune.py
+++ multilabel/fine_tune.py
@@ -6,7 +6,11 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
-def fine_tune_pretrained_model(base_model, df_train):
+def fine_tune_pretrained_model(
+    base_model,
+    df_train,
+    linear_probe_model_save_path,
+    fine_tuned_model_save_filepath):
 
     # linear probe
     linear_probe = Dense(14, activation="sigmoid")
@@ -47,7 +51,10 @@ def fine_tune_pretrained_model(base_model, df_train):
     base_model.trainable = False
 
     # compile model
-    model.compile(optimizer=Adam(learning_rate=0.001), loss="binary_crossentropy", metrics=[AUC(multi_label=True), Precision(), Recall()])
+    model.compile(
+        optimizer=Adam(learning_rate=0.001),
+        loss="binary_crossentropy",
+        metrics=[AUC(multi_label=True), Precision(), Recall()])
 
     # train model
     model.fit(
@@ -58,7 +65,7 @@ def fine_tune_pretrained_model(base_model, df_train):
         epochs=2,
         callbacks=[
             ModelCheckpoint(
-                filepath="data/xception_linear_probe.h5",
+                filepath=linear_probe_model_save_path,
                 verbose=1,
                 save_best_only=True)])
     
@@ -81,27 +88,7 @@ def fine_tune_pretrained_model(base_model, df_train):
         callbacks=[
                    EarlyStopping(patience=5, restore_best_weights=True),
                    ModelCheckpoint(
-                       filepath="data/xception_linear_probe_fine_tuned.h5",
+                       filepath=fine_tuned_model_save_filepath,
                        verbose=1,
                        save_best_only=True),
                    ReduceLROnPlateau(patience=1, verbose=1, cooldown=2)])
-
-
-if __name__ == "__main__":
-
-    from extract_features import get_train_test_dfs
-
-    from tensorflow.keras.applications import Xception
-
-    df_train, _ = get_train_test_dfs()
-
-    print(df_train)
-
-
-    base_model = Xception(
-        include_top=False,
-        weights="imagenet",
-        input_shape=(299,299,3),
-        pooling="max")
-
-    fine_tune_pretrained_model(base_model, df_train)
---
+++ multilabel/fine_tune_xception.py
@@ -0,0 +1,22 @@
+from extract_features import get_train_test_dfs
+from fine_tune import fine_tune_pretrained_model
+
+from tensorflow.keras.applications import Xception
+
+
+base_model = Xception(
+        include_top=False,
+        weights="imagenet",
+        input_shape=(299,299,3),
+        pooling="max")
+
+linear_probe_model_save_path = "data/xception_linear_probe.h5"
+fine_tuned_model_save_path = "data/xception_fine_tuned.h5"
+
+df_train, _ = get_train_test_dfs()
+
+fine_tune_pretrained_model(
+    base_model,
+    linear_probe_model_save_path,
+    fine_tuned_model_save_path,
+    df_train)
