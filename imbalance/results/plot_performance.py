9199fee39d608d2452c23754ab3d36003625f841
stergiosbamp <bampakis.stergios@gmail.com>
2021-05-16 23:27:19 +0300

    Function to plot in line the before and after balancing performance for each metric

--- imbalance/results/plot_performance.py
+++ imbalance/results/plot_performance.py
@@ -26,9 +26,33 @@ def plot_best_combo_per_metric(df):
     fig.show()
 
 
+def plot_before_after(df_imb, df_bal, model_idx):
+    for idx in df_imb.index:
+        if model_idx in idx:
+            metrics = df_imbal.loc[idx].index.to_list()
+            value_metrics_imb = df_imbal.loc[idx].to_list()
+
+    all_metrics_combos_bal = pd.DataFrame()
+    for idx in df_bal.index:
+        if model_idx in idx:
+            temp_metrics = df_bal.loc[idx]
+            all_metrics_combos_bal = all_metrics_combos_bal.append(temp_metrics)
+
+    value_metrics_bal = all_metrics_combos_bal.max().to_list()
+
+    data = {}
+    for metric, value_before, value_after in zip(metrics, value_metrics_imb, value_metrics_bal):
+        data[metric] = [value_before, value_after]
+    display_df = pd.DataFrame(data=data, index=['Before', 'After'])
+    fig = px.line(display_df, color_discrete_sequence=px.colors.qualitative.Safe)
+    fig.show()
+
+
 if __name__ == '__main__':
     df_imbal = populated_metrics_df(directory='./imbalance')
-    plot_imbalance(df_imbal)
+    # plot_imbalance(df_imbal)
 
     df_bal = populated_metrics_df(directory='./balance')
-    plot_best_combo_per_metric(df_bal)
+    # plot_best_combo_per_metric(df_bal)
+
+    plot_before_after(df_imbal, df_bal, 'MLP')
