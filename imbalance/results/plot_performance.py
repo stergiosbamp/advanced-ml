3c4ee671530af6aff2778a2e19f3490f7f32782a
stergiosbamp <bampakis.stergios@gmail.com>
2021-05-16 17:06:30 +0300

    Add plotting best combo sampling+model per each metric

--- imbalance/results/plot_performance.py
+++ imbalance/results/plot_performance.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import seaborn as sns
+import plotly.graph_objects as go
 import plotly.express as px
 
 
@@ -11,6 +11,22 @@ def plot_imbalance(df):
     fig.show()
 
 
+def plot_best_combo_per_metric(df):
+    metrics = df.max().index.to_list()
+    values_per_metric = df.max().to_list()
+    models_per_metric = df.idxmax().to_list()
+
+    fig = go.Figure()
+
+    for metric, value, model in zip(metrics, values_per_metric, models_per_metric):
+        fig.add_trace(go.Bar(name=model, x=[metric], y=[value], ))
+
+    fig.show()
+
+
 if __name__ == '__main__':
     df_imbal = populated_metrics_df(directory='./imbalance')
     plot_imbalance(df_imbal)
+
+    df_bal = populated_metrics_df(directory='./balance')
+    plot_best_combo_per_metric(df_bal)
