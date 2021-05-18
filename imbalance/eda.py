ad6320708d67538be911517ec769e7b77a9425be
stergiosbamp <bampakis.stergios@gmail.com>
2021-05-18 22:10:37 +0300

    Fix in eda sequential plots

--- imbalance/eda.py
+++ imbalance/eda.py
@@ -19,38 +19,38 @@ class EDA:
     def pie_chart_class_distr(self):
         # Replace numbers with their meaning for visualization reasons
         fetal_health_classes = self.df[self.target]
-        fetal_health_classes.replace({
+        fetal_health_classes = fetal_health_classes.replace({
             1: 'Normal',
             2: 'Suspect',
             3: 'Pathological'
-        }, inplace=True)
+        })
 
         title = 'Class distribution of fetal Cardiotocograms'
 
-        fig = px.pie(fetal_health_classes, names=self.target, hole=0.2, title=title, width=800, height=800,
+        fig = px.pie(fetal_health_classes, names=self.target, hole=0.2, title=title, width=650, height=650,
                      color_discrete_sequence=px.colors.qualitative.Safe)
         fig.show()
 
     def bar_chart_class_distr(self):
         # Replace numbers with their meaning for visualization reasons
         fetal_health_classes = self.df[self.target]
-        fetal_health_classes.replace({
+        fetal_health_classes = fetal_health_classes.replace({
             1: 'Normal',
             2: 'Suspect',
             3: 'Pathological'
-        }, inplace=True)
+        })
         counts = fetal_health_classes.value_counts()
-        fig = px.bar(data_frame=counts, color_discrete_sequence=px.colors.qualitative.Safe)
+        fig = px.bar(data_frame=counts, color_discrete_sequence=px.colors.qualitative.Safe, width=650, height=650)
         fig.show()
 
     def box_plot(self):
         df_unscaled = self.df
-        fig = px.box(df_unscaled)
+        fig = px.box(data_frame=df_unscaled, color_discrete_sequence=px.colors.sequential.Aggrnyl)
         fig.show()
 
     def corr_heatmap(self):
         corr = df.corr()
-        fig = px.imshow(corr, color_continuous_scale=px.colors.sequential.YlGnBu)
+        fig = px.imshow(img=corr, color_continuous_scale=px.colors.sequential.YlGnBu)
         fig.show()
 
 
